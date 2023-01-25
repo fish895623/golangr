79b12f6a68557c064fb87ad6f8d8360384d25e05
fish895623 <dan990429@gmail.com>
2023-01-25 10:25:03 +0900

    test case

--- main_test.go
+++ main_test.go
@@ -1,29 +1,25 @@
 package main_test
 
 import (
-	"fmt"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
-	a "gitlab.com/fish895623/golangr"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPingRoute(t *testing.T) {
-	router := a.SetUpRouter()
-	gin.SetMode(gin.TestMode)
+	rPath := "/user"
+	router := gin.Default()
+	router.GET(rPath, func(c *gin.Context) {
+		c.String(http.StatusOK, "hello %s", "hello")
+	})
+	req, _ := http.NewRequest("GET", rPath, nil)
 
-	req, _ := http.NewRequest("GET", "/", nil)
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 
-	responseData, _ := io.ReadAll(w.Body)
-
-	fmt.Print(string(responseData))
-
-	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, http.StatusOK, w.Code, "response status should be 200")
+	assert.Equal(t, w.Body.String(), "hello hello")
 }
