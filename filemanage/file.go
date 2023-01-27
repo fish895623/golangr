aee6b3f394d47c3c9561f5ad3056894cfcff5468
fish895623 <dan990429@gmail.com>
2023-01-27 18:09:01 +0900

    create file if not exists

--- filemanage/file.go
+++ filemanage/file.go
@@ -7,7 +7,25 @@ func Readfile() string {
 	return string(dat)
 }
 
+func CheckFileExists() bool {
+	if _, err := os.Stat("../data/write"); err == nil {
+		return true
+	} else {
+		return false
+	}
+}
+
+func CreateFile() {
+	if _, e := os.Create("../data/write"); e != nil {
+		panic(e)
+	}
+}
+
 func Writefile(a string) {
+	if !CheckFileExists() {
+		CreateFile()
+	}
+
 	data := []byte(a)
 	os.WriteFile("../data/write", data, 0644)
 }
